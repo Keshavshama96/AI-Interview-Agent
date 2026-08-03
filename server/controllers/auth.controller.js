e1e9a2c4c9c121e5952732b759f8199462d4044b
keshav-Shama-07 <keshavshama1@gmail.com>
2026-08-03 23:05:04 +0530

    Update auth.controller.js

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -13,7 +13,7 @@ export const googleAuth = async (req, res) => {
         }
         let token = await genToken(user._id)
 res.cookie("token", token, {
-    http: true,
+    httpOnly: true,
     secure: true,
     sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000
@@ -28,9 +28,19 @@ return res.status(200).json(user)
 
 export const logOut = async (req, res) => {
     try {
-        await res.clearCookie("token")
-        return res.status(200).json({message: "LogOut Successfully"})
+        res.clearCookie("token", {
+            httpOnly: true,
+            secure: true,
+            sameSite: "none"
+        });
+
+        return res.status(200).json({
+            message: "LogOut Successfully"
+        });
+
     } catch (error) {
-        return res.status(500).json({message: `Logout error ${error}`})
+        return res.status(500).json({
+            message: `Logout error ${error}`
+        });
     }
 }
